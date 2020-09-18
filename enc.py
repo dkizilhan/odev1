0c6a582e4a6f343f5a4658cdd6ed5a639324b561
dkizilhan <68823119+dkizilhan@users.noreply.github.com>
2020-09-19 01:11:26 +0300

    Add files via upload

---
+++ enc.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import String
+from textwrap import wrap
+
+
+
+def callbackdrive(data):
+	mydata = str(data.data)
+	if len(mydata) == 18:
+		edit(mydata)
+	
+
+def callbackarm(data):
+	mydata = str(data.data)
+	if len(mydata) == 26:
+		edit(mydata)
+
+
+def edit(data):
+	if mydata[0] == 'A' and mydata[-1] == 'B':
+		datanum1 = str(mydata[1:-1])
+		septdata1 = int(wrap(datanum1,4))
+
+		for i in septdata1:
+			if septdata1[i] > 1250:
+				septdata1[i] = 1250
+			elif 255 < septdata1[i] < 1000:
+				septdata1[i] = 255
+			if septdata1[i] > 1000:
+				septdata1[i] = septdata[i] - 1000
+			elif septdata1[i] < 1000:
+				septdata1[i] = -septdata[i]
+
+			lastdata1[i] = str(septdata1[i])
+
+	return lastdata1
+
+
+
+def listen():
+	rospy.init_node('listen', anonymous=True)
+	rospy.Subscriber("serial/drive", String, self.callbackdrive)
+	rospy.Subscriber("serial/robotic_arm", String, self.callbackarm)
+	rospy.spin()
+
+
+def publish():
+	rospy.init_node('publish', anonymous=True)
+	pubdrive = rospy.Publisher("position/drive", String, queue_size = 10)
+    pubarm = rospy.Publisher("position/robotic_arm", String, queue_size = 10)
+    rate = rospy.Rate(10)
+
+    while not rospy.is_shutdown():
+    	rospy.loginfo(lastdata1)
+
+
+if __name__ == "__main__":
+	listen()
+	publish()
